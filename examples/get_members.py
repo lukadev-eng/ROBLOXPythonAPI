7d561674b6d4241defe1c829f941be7662fe38da
lukadev-eng <lolpuuhapete@gmail.com>
2025-05-28 18:39:35 +0300

    Create get_members.py

---
+++ examples/get_members.py
@@ -0,0 +1,13 @@
+import robloxapi, asyncio
+client = robloxapi.Client("COOKIE")
+
+
+"""
+Gets all users of a group.
+"""
+async def main():
+  group = await client.get_group(2695946)
+  async for member in group.get_members():
+    print(f"{member.name} has role {member.role.name}")
+
+asyncio.run(main())
